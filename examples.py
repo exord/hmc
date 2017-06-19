5e8cfdb0852acd88996a1659fa7f2a4182dd5390
Rodrigo F. Díaz <rodrigo.diaz@unige.ch>
2017-06-19 15:14:56 -0300

    Added plot to example

--- examples.py
+++ examples.py
@@ -4,7 +4,9 @@ import pylab as plt
 import hmc.hmcsampler as hh
 import gradients
 
-niterations = 5000
+niterations = 2500
+
+np.random.seed(1062005)
 
 # Drawing from mixture
 mu1 = [0,0]
@@ -27,15 +29,30 @@ f1 = plt.figure()
 ax = f1.add_subplot(111)
 ax.set_aspect('equal')
 ax.plot(chain[:, 0], chain[:, 1], '.k')
+ax.set_xlabel(r'$\theta_1$')
+ax.set_ylabel(r'$\theta_2$')
 
 f2 = plt.figure()
 ax2 = f2.add_subplot(111)
 ax2.set_aspect('equal')
+ax2.set_xlabel(r'$\theta_1$')
+ax2.set_ylabel(r'$\theta_2$')
 
 f3 = plt.figure()
 ax3 = f3.add_subplot(111)
+ax3.set_xlabel('Iteration')
+ax3.set_ylabel('H/<H>')
 
 for i in np.random.randint(niterations, size=20):
     ax2.plot(q[i, :, 0], q[i, :, 1], '.-')
     ax3.plot(h[i]/h[i].mean())
+
+f4 = plt.figure()
+ax4 = f4.add_subplot(111)
+ax4.set_xlabel('Iteration')
+ax4.set_ylabel(r'$\theta_1$ / $\theta_2$')
+
+ax4.plot(chain[:, 1], '-', color='0.5', label=r'$\theta_2$')
+ax4.plot(chain[:, 0], '-k', label=r'$\theta_1$')
+ax4.legend(loc=0)
 
